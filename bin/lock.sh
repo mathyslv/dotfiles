14fc6edc250b8cffb5ca09aea6fec805dd32f06c
vhamstyl <167907295+vhamstyl@users.noreply.github.com>
2024-04-27 14:54:47 +0200

    feat: use betterlockscreen

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,42 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-BLANK='#00000000'
-CLEAR='#ffffff22'
-DEFAULT='#000000cc'
-TEXT='#0000eeee'
-WRONG='#AA0000bb'
-VERIFYING='#00bbbbbb'
+IMAGE="/tmp/lock.png"
+BLUR_GEOMETRY="5x2"
+BLUR_ANGLE=5
 
-i3lock \
---nofork                    \
---insidever-color=$CLEAR    \
---ringver-color=$VERIFYING  \
-\
---insidewrong-color=$CLEAR  \
---ringwrong-color=$WRONG    \
-\
---inside-color=$BLANK       \
---ring-color=$DEFAULT       \
---line-color=$BLANK         \
---separator-color=$DEFAULT  \
-\
---verif-color=$TEXT         \
---wrong-color=$TEXT         \
---time-color=$TEXT          \
---date-color=$TEXT          \
---keyhl-color=$TEXT         \
---bshl-color=$WRONG         \
-\
---radius 175                \
---ring-width 10             \
-\
--c "#00000033"              \
---clock                     \
---indicator                 \
---time-str="%H:%M:%S"       \
---time-size=48              \
---date-str="%d/%m/%Y"       \
---date-size=24              \
---ind-pos='x+(w/2):y+(h/2)' \
---time-pos='ix:iy'          \
---date-pos='ix:iy+40'
+rm -f "${IMAGE}"
+scrot "${IMAGE}"
+convert "${IMAGE}" -scale 10% -scale 1000% -radial-blur "${BLUR_ANGLE}" -blur "${BLUR_GEOMETRY}" "${IMAGE}"
+betterlockscreen -u "${IMAGE}" --fx "pixel" --pixel 10,10 --lock -- --no-verify
