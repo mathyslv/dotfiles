ec27011a14f543cdd671319baa14d0ea33efdd3a
vhamstyl <167907295+vhamstyl@users.noreply.github.com>
2024-04-27 15:01:25 +0200

    feat: use betterlockscreen

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 IMAGE="/tmp/lock.png"
-BLUR_GEOMETRY="5x2"
+BLUR_GEOMETRY="2x8"
 BLUR_ANGLE=5
 
 rm -f "${IMAGE}"
 scrot "${IMAGE}"
 convert "${IMAGE}" -scale 10% -scale 1000% -radial-blur "${BLUR_ANGLE}" -blur "${BLUR_GEOMETRY}" "${IMAGE}"
-betterlockscreen -u "${IMAGE}" --fx "pixel" --pixel 10,10 --lock
+betterlockscreen --fx "" -u "${IMAGE}" --lock
